db6096a00a82e86baa1d64824e046b202b8e36c5
dragos.geornoiu <dragosgeornoiu@gmail.com>
2024-02-14 13:34:18 +0200

    Internationalization for footer.

--- couses-typescript/src/scenes/footer/index.tsx
+++ couses-typescript/src/scenes/footer/index.tsx
@@ -1,29 +1,29 @@
 import Logo from "@/assets/Logo.png"
+import { useTranslation } from 'react-i18next';
 
 const Footer = () => {
+  const { t } = useTranslation();
   return <footer className="bg-primary-100 py-16">
     <div className="justify-content mx-auto w-5/6 gap-16 md:flex">
         <div className="mt-16 basis-1/2 md:mt-0">
             <img alt='logo' src={Logo} />
             <p className="my-5">
-                ABFdsa fdsafasd fdsafnaf ABFdsa fdsafasd fdsafnaf ABFdsa fdsafasd fdsafnaf 
-                ABFdsa fdsafasd fdsafnaf ABFdsa fdsafasd fdsafnaf ABFdsa fdsafasd fdsafnaf 
-                ABFdsa fdsafasd fdsafnaf ABFdsa fdsafa.
+            {t(`footer.paragraph`)}
             </p>
-            <p>© Evogym All Rights Reserved</p>
+            <p>{t(`footer.copyright`)}</p>
         </div>
 
         <div className="mt-16 basis-1/4 md:mt-0">
-            <h4 className="font-bold">Links</h4>
-            <p className="my-5">MASSA orci senectus</p>
-            <p className="my-5">Et gravid id et etiam</p>
-            <p>Ullamcorper bibamus</p>
+            <h4 className="font-bold">{t(`footer.link.title`)}</h4>
+            <p className="my-5">{t(`footer.link.first`)}</p>
+            <p className="my-5">{t(`footer.link.second`)}</p>
+            <p>{t(`footer.link.third`)}</p>
         </div>
 
         <div className="mt-16 basis-1/4 md:mt-0">
-            <h4 className="font-bold">Contact Us</h4>
-            <p className="my-5">Tempus metus mattis rissus voluptat egestas.</p>
-            <p>(333)425-6825</p>
+            <h4 className="font-bold">{t(`footer.contactus.title`)}</h4>
+            <p className="my-5">{t(`footer.contactus.paragraph.first`)}</p>
+            <p>{t(`phoneNumber`)}</p>
         </div>
     </div>
 
